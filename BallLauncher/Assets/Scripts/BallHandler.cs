d32b987be4a1cf351394ccd96e814d96a07dadc3
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2022-07-02 21:41:48 +0200

    Add launch ball Add detach ball

--- BallLauncher/Assets/Scripts/BallHandler.cs
+++ BallLauncher/Assets/Scripts/BallHandler.cs
@@ -5,9 +5,13 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    private Camera mainCamera;
     [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] public float detachDelay;
 
+    private Camera mainCamera;
+    private bool isDragging;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,20 +21,42 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentBallRigidbody == null)
+        {
+            return;
+        }
+        
         if(!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRigidbody.isKinematic = false;
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+
+            isDragging = false;
             return;
         }
 
+        isDragging = true;
         currentBallRigidbody.isKinematic = true;
         
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        Vector3 worldTouchPosition2 = mainCamera.ScreenToWorldPoint(touchPosition);
-        
-        Debug.Log(touchPosition);
-        Debug.Log(worldTouchPosition2);
+        Vector3 worldPosition2 = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        currentBallRigidbody.position = worldTouchPosition2;
+        currentBallRigidbody.position = worldPosition2;
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+        
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
