71fd26ced4ebe0e02ffcf4822a39a1d6179e827e
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2022-07-12 15:59:36 +0200

    Add a "car" with initial movement

---
+++ DaCarrinhoNao/Assets/Scripts/Car.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float speedGainPerSecond = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += speedGainPerSecond * Time.deltaTime;
+        
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        
+    }
+}
