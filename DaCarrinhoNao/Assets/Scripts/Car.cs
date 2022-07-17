b0cf36a89bd079f5e343b50892a41247f99858c7
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2022-07-17 17:00:54 +0200

    Add initial car control - 24.Steeing The Car

--- DaCarrinhoNao/Assets/Scripts/Car.cs
+++ DaCarrinhoNao/Assets/Scripts/Car.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
     [SerializeField] private float speedGainPerSecond = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
 
-    // Update is called once per frame
+    private int steerValue;
+    
     void Update()
     {
         speed += speedGainPerSecond * Time.deltaTime;
         
-        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
         
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
