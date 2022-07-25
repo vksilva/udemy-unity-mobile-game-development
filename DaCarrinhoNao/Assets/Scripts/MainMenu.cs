ca695384ad6264c4c6c82c351480ace96936cec8
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2022-07-25 20:54:48 +0200

    Add energy system

--- DaCarrinhoNao/Assets/Scripts/MainMenu.cs
+++ DaCarrinhoNao/Assets/Scripts/MainMenu.cs
@@ -6,17 +6,58 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
 
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
         
         highScoreText.text = $"High Score:\n {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+            
+            if (energyReadyString == String.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+            
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+        }
+
+        energyText.text = $"Play ({energy})";
     }
 
     public void Play()
     {
+        if (energy < 1) return;
+        
         //Load game scene
         SceneManager.LoadScene(1);
+        ConsumeEnergy();
+
+        if (energy != 0) return;
+        
+        DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+        PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+    }
+
+    private void ConsumeEnergy()
+    {
+        energy--;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+        
+        energyText.text = $"Play ({energy})";
     }
 }
