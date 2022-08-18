22cc67f6623eaa3a5a0a6756d4bfda57ef035f9a
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2022-08-18 15:42:55 +0200

    Make the ship don't leave visible screen area

--- AsteroidAvoider/Assets/Scripts/PlayerMovement.cs
+++ AsteroidAvoider/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -22,6 +23,51 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(newPosition);
+        
+        if (viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+
+        if (viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+
+        if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(movementDirection == Vector3.zero){return;}
+        
+        playerRigidbody.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        playerRigidbody.velocity = Vector3.ClampMagnitude(playerRigidbody.velocity, maxVelocity);
+    }
+
+    private void ProcessInput()
+    {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             //Detect input position
@@ -37,14 +83,5 @@ public class PlayerMovement : MonoBehaviour
         {
             movementDirection = Vector3.zero;
         }
-    }
-
-    private void FixedUpdate()
-    {
-        if(movementDirection == Vector3.zero){return;}
-        
-        playerRigidbody.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
-
-        playerRigidbody.velocity = Vector3.ClampMagnitude(playerRigidbody.velocity, maxVelocity);
     }
 }
