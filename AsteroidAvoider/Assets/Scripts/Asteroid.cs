d4bc442884302ce8583662b97f91b0c439f28be9
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2022-08-19 14:52:16 +0200

    Add an asteroid and the logic to collid and being destroied by the asteroid

---
+++ AsteroidAvoider/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+        
+        if(playerHealth == null){return;}
+        
+        playerHealth.Crash();
+    }
+}
